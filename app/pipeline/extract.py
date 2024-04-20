e79bc30ee2a26f0fffb1027407a91cd4a6a4417d
Rafael Jürkfitz <rafaeljurkfitz@hotmail.com>
2024-04-20 16:41:46 -0300

    fix extract file

--- app/pipeline/extract.py
+++ app/pipeline/extract.py
@@ -1,3 +1,5 @@
+"""module for extracting the necessary data to consolidate opening data."""
+
 import glob  # library to list files
 import os  # library to manipulate files and folders
 from typing import List
@@ -6,16 +8,17 @@ import pandas as pd
 
 
 def extract_from_excel(path: str) -> List[pd.DataFrame]:
+    """Read the files of a folder ```data/input``` and return a list of dataframes.
+
+    Args:
+        input_path (str): Path to folder with the files
+
+    Returns:
+        list: list of dataframes
     """
-    A function to read the files of a folder ```data/input``` and return a list of
-    dataframes
-
-    args: input_path (str): path to folder with the files
-
-    return: list: list of dataframes
-    """
-
     all_files = glob.glob(os.path.join(path, '*.xlsx'))
+    if not all_files:
+        raise ValueError('No Excel files found in the specified folder')
 
     data_frame_list = []
     for file in all_files:
