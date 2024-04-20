efdb0a9c82d9a334bc121cf8050eb3eb4f8562b0
Rafael Jürkfitz <rafaeljurkfitz@hotmail.com>
2024-04-20 16:54:03 -0300

    fix the transform file

--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -1,11 +1,19 @@
+"""module with all the transformations necessary to consolidate the opening data."""
+
 from typing import List
 
 import pandas as pd
 
 
 def concat_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
-    """
-    Function to transform a list of dataframes in only one dataframe
-    """
+    """Transform a list of dataframes in only one dataframe.
 
+    Args:
+        data_frame_list (List[pd.DataFrame]): list of dataframes
+
+    Returns:
+        pd.DataFrame: one dataframe
+    """
+    if not data_frame_list:
+        raise ValueError('No data to transform')
     return pd.concat(data_frame_list, ignore_index=True)
